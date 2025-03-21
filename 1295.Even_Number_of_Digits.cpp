ac4a6c327bdf8891619310161f76cfd25eec6b23
tanmoykdas <tnmy468@gmail.com>
2025-03-21 21:36:48 +0600

    3/21/25

---
+++ 1295.Even_Number_of_Digits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public:
+        int findNumbers(vector<int>& nums) {\
+            int c = 0;
+            for(int i = 0; i < nums.size(); i++) {
+                string s = to_string(nums[i]);
+                if(s.size() % 2 == 0) c++;
+            }
+            return c;
+        }
+    };
